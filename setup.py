951ff25046be4abecdb3cc611b671bde746ff721
Emil Wåreus <emil.wareus47@gmail.com>
2020-03-24 08:17:21 +0100

    version bumb

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="smaland", 
-    version="0.0.3",
+    version="0.0.4",
     author="Emil Wåreus",
     author_email="emil.wareus47@gmail.com",
     description="Python Avanza API",
